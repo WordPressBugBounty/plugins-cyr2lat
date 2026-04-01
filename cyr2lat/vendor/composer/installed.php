981f50a80c60f8008be8f30cda85e3ad148ff1cb
Sean Murphy <sean@iamseanmurphy.com>
2026-04-01 21:27:46 +0000

    6.7.0

--- cyr2lat/vendor/composer/installed.php
+++ cyr2lat/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'mihdan/cyr2lat',
-        'pretty_version' => '6.6.0',
-        'version' => '6.6.0.0',
-        'reference' => '22508139907ff6db0ba1868cfbcfebb112bb9b69',
+        'pretty_version' => '6.7.0',
+        'version' => '6.7.0.0',
+        'reference' => 'e88c8b72755e692ca2109d60dcb6df3d4b35ccf8',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'mihdan/cyr2lat' => array(
-            'pretty_version' => '6.6.0',
-            'version' => '6.6.0.0',
-            'reference' => '22508139907ff6db0ba1868cfbcfebb112bb9b69',
+            'pretty_version' => '6.7.0',
+            'version' => '6.7.0.0',
+            'reference' => 'e88c8b72755e692ca2109d60dcb6df3d4b35ccf8',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
